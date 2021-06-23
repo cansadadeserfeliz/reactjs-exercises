1998cee0d6daac78cfd2308a9258d52c1f219c23
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-06-23 12:05:06 -0500

    Add Comment component

--- js/main.jsx
+++ js/main.jsx
@@ -1,11 +1,51 @@
-class Welcome extends React.Component {
-  render() {
-    return <h1>Здравствуй, {this.props.name}.</h1>;
-  }
+function Welcome(props) {
+  return <h1>Здравствуй, {props.name}.</h1>;
 }
 
-const name = 'Алиса';
-const element = <Welcome name={name} />;
+function Avatar(props) {
+  return (
+    <div className="flex-shrink-0">
+      <img src={props.user.avatarUrl} alt={props.user.name} />
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="d-flex">
+      <Avatar user={props.author} />
+      <div className="flex-grow-1 ms-3">
+        {props.text}
+        <div>
+          <i>{props.date.toISOString()}</i>
+        </div>
+      </div>
+    </div>
+  );
+}
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Алиса" />
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />
+    </div>
+  );
+}
+
+const element = <App />;
 
 // render a React element into a root DOM node
 ReactDOM.render(
